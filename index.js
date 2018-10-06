8544d0e648c7935c9e617fbf953d0a16aba9f095
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-06 16:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(kittens.length-1)
+  return kittens.slice(kittens.length - 1)
 }
 
 function removeFirstKitten() {
